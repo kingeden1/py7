df7507a221e5a75c09b8bd65b2575c19ec834676
Kingeden1 <stewjf@yahoo.com>
2025-02-27 20:49:26 +0300

    py7

---
+++ Activity2.py
@@ -0,0 +1,8 @@
+num= int(input("enter number:"))
+if num>=0:
+    if num==0:
+        print(num,"is zero itself")
+    else:
+        print(num,"is postive number ")
+else:
+    print(num,"is negative number")
